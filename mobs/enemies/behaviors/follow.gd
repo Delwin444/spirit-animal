1765bc51fcfd15fdec9b8823d0df3f982b620197
Delwin444 <justinkuhn@t-online.de>
2026-01-31 19:05:15 +0100

    Fix missing spirit gd

--- mobs/enemies/behaviors/follow.gd
+++ mobs/enemies/behaviors/follow.gd
@@ -4,7 +4,11 @@ class_name Follow
 
 func _physics_process(delta: float) -> void:
 	if noticed_player:
-		var directory = enemy.global_position.direction_to(GameState.player.global_position)
-		enemy.velocity = speed * directory * delta
+		var direction = enemy.global_position.direction_to(GameState.player.global_position)
+		
+		if not enemy.can_fly:
+			direction.y = 0
+			
+		enemy.velocity = speed * direction * delta
 	if not noticed_player:
 		enemy.velocity = Vector2.ZERO
---
+++ mobs/enemies/spirit/spirit.gd
@@ -0,0 +1,12 @@
+extends Enemy
+
+class_name Spirit
+
+func _ready() -> void:
+	notice_area = %NoticeArea
+	behavior.enemy = self
+	super()
+
+func _physics_process(delta: float) -> void:
+	super(delta)
+	move_and_slide()
