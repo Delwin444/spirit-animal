61902d0669e7bcfa5c46cb315a5d04a3a84db6ac
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2026-02-01 14:14:24 +0100

    Add move direction flipping the enemy texture

--- mobs/enemies/spirit/spirit.gd
+++ mobs/enemies/spirit/spirit.gd
@@ -1,6 +1,8 @@
 extends Enemy
-
 class_name Spirit
+
+@onready var visual: Node2D = $Node2D
+@onready var tile_map: TileMapLayer = $TileMapLayer
 
 func _ready() -> void:
 	notice_area = %NoticeArea
@@ -11,4 +13,8 @@ func _physics_process(delta: float) -> void:
 	super(delta)
 	move_and_slide()
 	handle_collisions()
-	
+	update_facing_direction()
+
+func update_facing_direction() -> void:
+	if velocity.x != 0:
+		visual.scale.x = -sign(velocity.x) * abs(visual.scale.x)
