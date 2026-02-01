9e2f6247bd9da5945218e7e99458888681fecd73
J3rmany <wunder367.buch@gmail.com>
2026-02-01 18:00:28 +0100

    Some Controll Fixes, not breaking anything here, trust!

--- main.gd
+++ main.gd
@@ -16,10 +16,11 @@ func _ready() -> void:
 
 
 func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("open_inventory"):
-		inventory.show()
-	if Input.is_action_just_pressed("ui_close_dialog") and inventory.visible == true:
+	if Input.is_action_just_pressed("open_inventory") and inventory.visible == true:
 		inventory.hide()
+	elif Input.is_action_just_pressed("open_inventory") and inventory.visible == false:
+		inventory.show()
+
 
 
 func _on_mask_added(mask: Mask) -> void:
--- ui/inventory_grid.gd
+++ ui/inventory_grid.gd
@@ -1,5 +1,7 @@
 extends GridContainer
 
+@onready var maxAmountOfMasks = 0
+
 func _ready():
 	GameState.mask_added.connect(add_mask_to_inventory)
 	
@@ -14,3 +16,20 @@ func add_mask_to_inventory(mask: Mask):
 	mask_button.stretch_mode = TextureButton.STRETCH_SCALE
 	
 	add_child(mask_button)
+	maxAmountOfMasks += 1
+
+func get_inv_controller_dir():
+	var inputRight = 0
+	var inputLeft = 0
+	var itemSelected = 0
+	
+	if Input.is_action_just_pressed("inv_right"):
+		inputRight = 1
+	
+	if Input.is_action_just_pressed("inv_left"):
+		inputLeft = 1
+	itemSelected = inputRight - inputLeft
+	if itemSelected <= 0: itemSelected = maxAmountOfMasks
+	elif itemSelected >= maxAmountOfMasks: itemSelected = 0
+	
+	return itemSelected
